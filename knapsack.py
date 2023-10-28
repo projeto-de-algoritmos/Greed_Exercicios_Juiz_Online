7bff1ade3f8bdf652aad4c2fe82994c1d986a03d
fellipepcs <fellipy.02@hotmail.com>
2023-10-28 11:22:19 -0300

    resolvendo problema da mochila. achando o ponto medio da capacidade da mochila

--- knapsack.py
+++ knapsack.py
@@ -37,3 +37,35 @@ class KnapsackSolver:
                 print(1)
                 print(position)
                 continue
+
+            item_positions = {}
+            total_weight = 0
+            selected_items = []
+
+            for i in range(num_items):
+                if items[i] in item_positions:
+                    item_positions[items[i]].append(i + 1)
+                else:
+                    item_positions[items[i]] = [i + 1]
+
+            found_mid_point = 1
+            for key in sorted(item_positions.keys()):
+                for i in range(len(item_positions[key])):
+                    total_weight += key
+                    if total_weight < mid_point:
+                        selected_items.append(item_positions[key][i])
+                    if mid_point <= total_weight <= weight:
+                        selected_items.append(item_positions[key][i])
+                        found_mid_point = 0
+                        break
+
+            if found_mid_point == 1:
+                print(-1)
+                continue
+
+            print(len(selected_items))
+            print(*selected_items)
+
+
+solver = KnapsackSolver()
+solver.solve()
