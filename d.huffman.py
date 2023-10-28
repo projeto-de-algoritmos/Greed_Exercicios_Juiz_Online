93a789f5319394575f3c9dd4171ddfe72768e294
fellipepcs <fellipy.02@hotmail.com>
2023-10-28 11:05:49 -0300

    adicionado custom remove para removar da sequencia de numeros

--- d.huffman.py
+++ d.huffman.py
@@ -15,3 +15,19 @@ class CustomQuery:
             return self.bucket < other.bucket
         return self.right < other.right
 
+def custom_huffman_coding(n, array, m, custom_queries):
+    results = [0] * m
+
+    def custom_remove(value, frequencies, bucket_frequencies, geq_root):
+        if frequencies[value] < SQRT:
+            bucket_frequencies[frequencies[value]] -= 1
+            frequencies[value] -= 1
+            bucket_frequencies[frequencies[value]] += 1
+        elif frequencies[value] == SQRT:
+            geq_root.remove(value)
+            bucket_frequencies[SQRT - 1] += 1
+            frequencies[value] -= 1
+        else:
+            frequencies[value] -= 1
+
+   
